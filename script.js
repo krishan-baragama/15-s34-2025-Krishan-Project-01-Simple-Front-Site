64f69c3d461ffeb96731ab5a32a22f7ebe617f0d
Krishan Baragama Acharige <krishan.baragama-acharige@epita.fr>
2025-09-19 22:07:47 +0200

    Implement the Js function

---
+++ script.js
@@ -0,0 +1,11 @@
+// select the toggle button
+const toggleBtn = document.querySelector(".header__toggle-btn")
+// select the header
+const header = document.querySelector(".header")
+
+toggleBtn.addEventListener("click", () => {
+	// add or remove the "header-open" class from the header element
+	// write your code here
+	console.log("run the function here !!")
+	header.classList.toggle("header-open")
+})
